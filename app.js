b82abaee833ab59114c4f938248fec6ba6e7c55f
Angus Lowe <anguslowe@live.ca>
2017-01-07 14:13:41 -0500

    added admin capabilities

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var firebase = require('firebase');
+
 var config = {
     apiKey: "AIzaSyDuf3gqHFDVEwkXveDlVT7jrW58MzdnR7Q",
     authDomain: "chirp-up.firebaseapp.com",
@@ -6,5 +7,12 @@ var config = {
     storageBucket: "chirp-up.appspot.com",
     messagingSenderId: "66307837599"
 };
+
+var admin = require("firebase-admin");
+
+admin.initializeApp({
+  credential: admin.credential.cert("path/to/serviceAccountKey.json"),
+  databaseURL: "https://chirp-up.firebaseio.com"
+});
 
 firebase.initializeApp(config);
