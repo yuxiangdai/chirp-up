56455f74d4010ea8c6695ba84e8e7d3e0d1aadf1
Angus Lowe <anguslowe@live.ca>
2017-01-07 14:10:37 -0500

    firebase added

---
+++ app.js
@@ -0,0 +1,10 @@
+var firebase = require('firebase');
+var config = {
+    apiKey: "AIzaSyDuf3gqHFDVEwkXveDlVT7jrW58MzdnR7Q",
+    authDomain: "chirp-up.firebaseapp.com",
+    databaseURL: "https://chirp-up.firebaseio.com",
+    storageBucket: "chirp-up.appspot.com",
+    messagingSenderId: "66307837599"
+};
+
+firebase.initializeApp(config);
