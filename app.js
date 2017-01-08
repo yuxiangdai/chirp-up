c9f0bfc82bdebfa43078a5736dec3abaccc804be
Charlie Yang <sdy1130@wifihost-95-62.wireless.utsc.utoronto.ca>
2017-01-07 22:32:54 -0500

    initialcommit

--- app.js
+++ app.js
@@ -7,12 +7,44 @@ app.set('view engine', 'ejs');
 var config = {
     apiKey: "AIzaSyDuf3gqHFDVEwkXveDlVT7jrW58MzdnR7Q",
     authDomain: "chirp-up.firebaseapp.com",
-    databaseURL: "https://chirp-up.firebaseio.com",
+    databaseURL: "https://chirp-up.firebaseio.com/",
     storageBucket: "chirp-up.appspot.com",
     messagingSenderId: "66307837599"
 };
 
 var admin = require("firebase-admin");
+var database = firebase.database();
+
+function initializeDatabase(userId){
+
+  firebase.database().ref('users/' + userID).set({
+    "responses":{
+      "topic":{
+          "politics":{
+              "question": {
+                  "text": "Do you think trump is a good president?",
+                  "yesno": "",
+                  "reasoning": ""
+              }        
+          },
+          "nutrition":{
+              "question": {
+                  "text": "Would you eat genetically modified food?",
+                  "yesno": "",
+                  "reasoning": ""
+              }
+          },
+          "life":{
+              "question": {
+                  "text": "Are you satistfied with your life?",
+                  "yesno": "",
+                  "reasoning": ""
+          }
+      }
+  }
+  }
+  })
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(__dirname + "/chirp-up-firebase-adminsdk-ftpw7-139d5bd98b.json"),
@@ -20,6 +52,7 @@ admin.initializeApp({
 });
 
 firebase.initializeApp(config);
+initializeDatabase("sdy1130");
 
 app.get('/', function(req, res) {
    res.render('home');
