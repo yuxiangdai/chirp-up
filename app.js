b94f1124e55fc26a883aec90b64f94ceccbce21a
Angus Lowe <anguslowe@live.ca>
2017-01-08 00:51:41 -0500

    fitted ejs view engine

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.get('/', function(req,res) {
-   res.sendFile(__dirname + "/public/index.html")
+   res.render('index');
 });
 
 app.listen(process.env.PORT || 3000, function() {
